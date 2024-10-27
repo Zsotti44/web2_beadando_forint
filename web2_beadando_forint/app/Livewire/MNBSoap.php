241782e841067a58ca6cdd3341e63321e47c08b8
Zsotti44 <zsotti44@gmail.com>
2024-10-27 17:56:35 +0100

    MNBSOAP refactor

--- web2_beadando_forint/app/Livewire/MNBSoap.php
+++ web2_beadando_forint/app/Livewire/MNBSoap.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Livewire;
-
 use App\Services\MNBService;
 use Livewire\Component;
 use Carbon\Carbon;
@@ -12,68 +11,25 @@ class MNBSoap extends Component
     public $date;
     public $year;
     public $month;
-    public $dailyRate = [];
+    public $dailyRate;
     public $monthlyRates = [];
-    public $availableCurrencies;
+    public $availableCurrencies = [];
 
     public function mount(MNBService $service)
     {
         $this->date = Carbon::now()->format('Y-m-d');
-        $availableCurrencies = $response = $service->call('GetCurrencies');
-        $xml = new \SimpleXMLElement($availableCurrencies->GetCurrenciesResult);
-
-        foreach ($xml->Currencies->Curr as $currency) {
-            $this->availableCurrencies[] = (string) $currency;
-        }
-
+        $this->availableCurrencies = $service->GetAllCurrencies();
     }
 
-    // Napi árfolyam lekérdezése
 
     public function getDailyRate(MNBService $service)
     {
-        $params = [
-            'startDate' => $this->date,
-            'endDate' => $this->date,
-            'currencyNames' => $this->currency,
-        ];
-
-        try {
-            $response = $service->call('GetExchangeRates', [$params]);
-            $xml = simplexml_load_string($response->GetExchangeRatesResult);
-            $this->dailyRate = (string)$xml->Day->Rate;
-        } catch (\Exception $e) {
-            $this->dailyRate = 'Hiba: ' . $e->getMessage();
-        }
+    return $this->dailyRate = $service->GetDailyRate($this->date, $this->currency);
     }
 
-    // Havi árfolyam lekérdezése
     public function getMonthlyRates(MNBService $service)
     {
-
-        $startDate = Carbon::create($this->year, $this->month, 1)->format('Y-m-d');
-        $endDate = Carbon::create($this->year, $this->month, 1)->endOfMonth()->format('Y-m-d');
-
-        $params = [
-            'startDate' => $startDate,
-            'endDate' => $endDate,
-            'currencyNames' => $this->currency,
-        ];
-
-        try {
-            $response = $service->call('GetExchangeRates', [$params]);
-            $xml = simplexml_load_string($response->GetExchangeRatesResult);
-
-            $this->monthlyRates = [];
-            foreach ($xml->Day as $day) {
-                $this->monthlyRates[] = [
-                    'date' => (string)$day['date'],
-                    'rate' => (string)$day->Rate,
-                ];
-            }
-        } catch (\Exception $e) {
-            $this->monthlyRates = [['error' => 'Hiba: ' . $e->getMessage()]];
-        }
+    return $this->monthlyRates = $service->GetMonthlyRates($this->currency,$this->year,$this->month);
     }
 
     public function render()
--- web2_beadando_forint/app/Services/MNBService.php
+++ web2_beadando_forint/app/Services/MNBService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+
 class MNBService
 {
     protected $client;
@@ -14,11 +16,69 @@ class MNBService
     public function call($functionName, $parameters = [])
     {
         try {
-            // Hívja meg a SOAP funkciót
             return $this->client->__soapCall($functionName, $parameters);
         } catch (\SoapFault $e) {
-            // Hibakezelés
             return 'SOAP Error: ' . $e->getMessage();
         }
     }
+
+    public function GetAllCurrencies()
+    {
+        $response = $this->call('GetCurrencies');
+        $xml = new \SimpleXMLElement($response->GetCurrenciesResult);
+        $temp = [];
+        foreach ($xml->Currencies->Curr as $currency) {
+            $temp[] = (string) $currency;
+        }
+        return $temp;
+    }
+    public function getDailyRate($date,$currency)
+    {
+        $params = [
+            'startDate' => $date,
+            'endDate' => $date,
+            'currencyNames' => $currency,
+        ];
+        $temp = '';
+
+        try {
+            $response = $this->call('GetExchangeRates', [$params]);
+            $xml = simplexml_load_string($response->GetExchangeRatesResult);
+            $temp = (string)$xml->Day->Rate;
+        } catch (\Exception $e) {
+            $temp = 'Hiba: ' . $e->getMessage();
+        }
+        return $temp;
+    }
+
+    // Havi árfolyam lekérdezése
+    public function getMonthlyRates($currency,$year,$month)
+    {
+
+        $startDate = Carbon::create($year, $month, 1)->format('Y-m-d');
+        $endDate = Carbon::create($year, $month, 1)->endOfMonth()->format('Y-m-d');
+
+        $params = [
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'currencyNames' => $currency,
+        ];
+
+        try {
+            $response = $this->call('GetExchangeRates', [$params]);
+            $xml = simplexml_load_string($response->GetExchangeRatesResult);
+
+            $temp = [];
+            foreach ($xml->Day as $day) {
+                $temp[] = [
+                    'date' => (string)$day['date'],
+                    'rate' => (string)$day->Rate,
+                ];
+            }
+        } catch (\Exception $e) {
+            $temp = [['error' => 'Hiba: ' . $e->getMessage()]];
+        }
+        return $temp;
+    }
+
 }
