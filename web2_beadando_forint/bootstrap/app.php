c42e71c471c69105a0eaeb5f042f297d9a464b8d
BobessA <adria72n@gmail.com>
2024-11-21 11:12:57 +0100

    leírás, /mnb CSRF kivételnek hogy kívülről is elérhető legyen

--- web2_beadando_forint/bootstrap/app.php
+++ web2_beadando_forint/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             '/soap',
+            '/mnb/*',
         ]);
         $middleware->alias([
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
